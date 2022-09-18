beac28a34b7a10cd20e4e223ae1caa309fe6e5fd
victor boddie <victorboddie21@gmail.com>
2022-09-18 12:43:33 -0400

    add heroku

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
+const PORT = process.env.PORT || 3001;
 
 app.get("/api/animals", (req, res) => {
   res.send("Hello!");
 });
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
 });
